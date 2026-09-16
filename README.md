f1e991911ade85f463c050a886c6e92477a0414a
Mohamed Gamal <support@flousflow.com>
2026-09-16 20:29:43 +0300

    Initial release: Odoo 19 internal account transfers

---
+++ README.md
@@ -0,0 +1,5 @@
+# Flous Flow Internal Account Transfers
+
+Provides paired bank/cash journal transfers for Odoo 19. This accounting-wide
+feature is intentionally separated from ConstructFlow so it can be installed,
+tested, and maintained independently.
---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Flous Flow Internal Account Transfers',
+    'version': '19.0.1.0.0',
+    'category': 'Accounting/Accounting',
+    'summary': 'Paired internal transfers between bank and cash journals',
+    'author': 'Flous Flow / Mohamed Gamal',
+    'license': 'OPL-1',
+    'depends': ['account'],
+    'data': [
+        'views/account_payment_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
